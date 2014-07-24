0b308fd637149ea9043faec8537cb77b891ca950
Tad Hardesty <tad@platymuus.com>
2014-07-23 21:35:06 -0500

    Scoreboards now associate with players and keep them updated on changes.

--- src/main/java/net/glowstone/GlowServer.java
+++ src/main/java/net/glowstone/GlowServer.java
@@ -172,7 +172,7 @@ public final class GlowServer implements Server {
     /**
      * The scoreboard manager for the server.
      */
-    private final ScoreboardManager scoreboardManager = new GlowScoreboardManager(this);
+    private final GlowScoreboardManager scoreboardManager = new GlowScoreboardManager(this);
 
     /**
      * The crafting manager for this server.
@@ -644,7 +644,7 @@ public final class GlowServer implements Server {
         return GlowItemFactory.instance();
     }
 
-    public ScoreboardManager getScoreboardManager() {
+    public GlowScoreboardManager getScoreboardManager() {
         return scoreboardManager;
     }
 
--- src/main/java/net/glowstone/entity/GlowPlayer.java
+++ src/main/java/net/glowstone/entity/GlowPlayer.java
@@ -23,6 +23,7 @@ import net.glowstone.net.message.play.game.*;
 import net.glowstone.net.message.play.inv.*;
 import net.glowstone.net.message.play.player.PlayerAbilitiesMessage;
 import net.glowstone.net.protocol.ProtocolType;
+import net.glowstone.scoreboard.GlowScoreboard;
 import net.glowstone.util.StatisticMap;
 import net.glowstone.util.TextWrapper;
 import org.apache.commons.lang.Validate;
@@ -213,6 +214,11 @@ public final class GlowPlayer extends GlowHumanEntity implements Player {
     private Location signLocation;
 
     /**
+     * The scoreboard the player is currently subscribed to.
+     */
+    private GlowScoreboard scoreboard;
+
+    /**
      * Whether the player is permitted to fly.
      */
     private boolean canFly;
@@ -291,6 +297,9 @@ public final class GlowPlayer extends GlowHumanEntity implements Player {
         sendWeather();
         sendAbilities();
 
+        scoreboard = server.getScoreboardManager().getMainScoreboard();
+        scoreboard.subscribe(this);
+
         invMonitor = new InventoryMonitor(getOpenInventory());
         updateInventory(); // send inventory contents
 
@@ -351,6 +360,10 @@ public final class GlowPlayer extends GlowHumanEntity implements Player {
         getInventory().removeViewer(this);
         getInventory().getCraftingInventory().removeViewer(this);
         permissions.clearPermissions();
+        if (scoreboard != null) {
+            scoreboard.unsubscribe(this);
+            scoreboard = null;
+        }
         super.remove();
     }
 
@@ -1431,11 +1444,20 @@ public final class GlowPlayer extends GlowHumanEntity implements Player {
     // Scoreboard
 
     public Scoreboard getScoreboard() {
-        return null;
+        return scoreboard;
     }
 
     public void setScoreboard(Scoreboard scoreboard) throws IllegalArgumentException, IllegalStateException {
-
+        Validate.notNull(scoreboard, "Scoreboard must not be null");
+        if (!(scoreboard instanceof GlowScoreboard)) {
+            throw new IllegalArgumentException("Scoreboard must be GlowScoreboard");
+        }
+        if (this.scoreboard == null) {
+            throw new IllegalStateException("Player has not loaded or is already offline");
+        }
+        this.scoreboard.unsubscribe(this);
+        this.scoreboard = (GlowScoreboard) scoreboard;
+        this.scoreboard.subscribe(this);
     }
 
     ////////////////////////////////////////////////////////////////////////////
